d05dcf7130a0edfce6b63e4f72e17aeaae34b946
byeongseon8-arch <byeongseon8@gamil.com>
2026-04-14 15:01:22 +0900

    Inconsistent merge state: could not find file MERGE_MSG in .git folder. This file contains the commit message for a merge commit.

--- src/CH00/C02Develop.java
+++ src/CH00/C02Develop.java
@@ -3,7 +3,9 @@ package CH00;
 public class C02Develop {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		// V0.1 Develop(조장:홍길동)
+		System.out.print("V0.1 Develop(조장 : 홍길동)");
 
 	}
 
